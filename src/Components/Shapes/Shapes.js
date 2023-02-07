3ad89e17607820c479557eb745408329fcf34ffa
mkmahmud <mahmudulhasanmk434@gmail.com>
2023-02-08 04:46:03 +0600

    about

--- src/Components/Shapes/Shapes.js
+++ src/Components/Shapes/Shapes.js
@@ -7,29 +7,29 @@ import { FaAngleRight } from 'react-icons/fa';
 
 const Shapes = () => {
     return (
-        <div className='relative mb-24' style={{ fontFamily: `'Poppins', sans-serif` }}>
-            <div className='md:h-[50vh] py-[20%] md:flex justify-between relative '>
-                <div className='w-1/3 relative rounded-full '>
+        <div className='relative md:my-48 h-screen ' style={{ fontFamily: `'Poppins', sans-serif` }}>
+            <div className='h-[1600px] md:h-screen pt-[40%] md:flex justify-between relative mt-[200px] md:mt-0 '>
+                <div className='w-full md:w-1/3 relative rounded-full  '>
                     {/* <img src={about} className='absolute top[-100px] left-[100px] z-10' alt="" /> */}
-                    <div className=' left-outter-shadow h-px w-px absolute top-[150px] left-64'>
+                    <div className=' left-outter-shadow h-px w-px absolute top-[50px] md:-top-[150px] left-[50%] md:left-64'>
                         <div className=' left-inner-shadow h-px w-px rounded-full'>
                         </div>
                     </div>
                 </div>
-                <div className='w-2/3 -z-10'>
-                    <div className='h-[300px] w-[300px] md:h-[1000px] md:w-[1000px] rounded-full bg-[#23cd9e87] absolute overflow-hidden top-0 -right-[100px]'>
+                <div className='w-full md:w-2/3 -z-10'>
+                    <div className='h-[1000px] w-[1000px] md:h-[1000px] md:w-[1000px] rounded-full bg-[#23cd9e87] absolute overflow-hidden  bottom-0 md:top-0 -right-[100px]'>
                         <div className='shadow-right-inner absolute top-[500px] left-[500px]'>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='h-full w-full md:px-10   absolute top-0 md:flex justify-around items-center '>
-                <div className='md:w-1/2  '>
-                    <img src={about} className='mt-48' alt="" />
+            <div className='h-full w-full px-2 md:px-10 absolute top-0 md:flex justify-around items-center  '>
+                <div className='w-full md:w-1/2  '>
+                    <img src={about} className=' mt-4 md:mt-48 block mx-auto' alt="" />
                     <Link to='/' className='flex items-center justify-center pt-24 text-[#FF5B00] text-lg font-bold'><span className='px-4'>Learn More</span> <FaAngleRight></FaAngleRight> </Link>
                 </div>
-                <div className='md:w-1/2'>
-                    <div className='text-center md:w-[500px] mx-auto mt-60  '>
+                <div className='w-full md:w-1/2'>
+                    <div className='text-center md:w-[500px] mx-auto mt-60 md:mt-2  '>
                         <div className='mt-24'>
                             <SectionTitle content='About Us'></SectionTitle>
                         </div>
--- src/Pages/Home/WhyChooseUs/WhyChooseUs.js
+++ src/Pages/Home/WhyChooseUs/WhyChooseUs.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const WhyChooseUs = () => {
     return (
-        <div className='my-24'>
+        <div className='my-96 md:my-0 bg-blue-400'>
             asdfghjk
         </div>
     );
